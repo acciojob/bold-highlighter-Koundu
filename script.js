1189db9cf4e3ccb0501f8a99302d270e2b14eec7
Koundu <121346044+Koundu@users.noreply.github.com>
2023-05-25 16:02:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function highlight() {
     //Write your code here
-
+	var x = document.getElementBtId("p").querySelectorAll(".strong");
+	x[0].style.color = "green";
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+		document.getElementById("strong").style.color="black";
     
 }
