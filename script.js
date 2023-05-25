6188871b3da0b2bf5d547ad303453e32f700139e
Koundu <121346044+Koundu@users.noreply.github.com>
2023-05-25 16:46:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-function highlight() {
-    //Write your code here
-	var x = document.getElementBtId("p").querySelectorAll(".strong");
-	x[0].style.backgroundColor = "green";
-
+function highlight(){
+	var x=document.getElementById("p").querySelectorAll("strong")
+	for(let i=0;i<x.length;i++){
+	x[i].style.color="green";
+	}
 }
 
+function return_normal(){	
+	var x=document.getElementById("p").querySelector("strong");
+	x[0].style.color = "black";
+	// for(let j=0;j<x.length;j++){
+	// x[j].style.color="black";
+	// }
+	console.log(x);
+	console.log("Inside Return Normal")
 
-function return_normal() {
-    //Write your code here
-		document.getElementById("strong").style.color="black";
-    
 }
